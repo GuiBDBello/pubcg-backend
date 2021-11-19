aa1513d5c2790dffcb3e6039331d1344c0006c9c
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2021-11-19 00:01:45 -0300

    Add aws-sdk and mysql2 packages; Update gameController

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -35,31 +35,19 @@ module.exports = {
         let logo = request.files.logo[0];
         let media = request.files.media;
         let file = request.files.file[0];
-        console.log('file', file);
-        console.log('logo', logo);
 
-        let originalFilename = file.originalname.split(".")[0];
-
-        let gameFilename = file.filename;
-        let logoFilename = logo.filename;
-        console.log('gameFilename', gameFilename);
-        console.log('logoFilename', logoFilename);
-
-        let filePath = path.resolve('uploads', gameFilename)
-        let logoPath = path.resolve('uploads', logoFilename);
-        console.log('filePath', filePath);
-        console.log('logoPath', logoPath);
-
-        let gameDirectory = gameFilename.split(".")[0];
+        let gameDirectory = file.filename.split(".")[0];
         console.log('gameDirectory', gameDirectory);
+
+        let logoFilename = logo.filename;
 
         let fileDestination = path.resolve('public', 'games', gameDirectory);
         let logoDestination = path.resolve('public', 'games', gameDirectory, logoFilename);
-        console.log('fileDestination', fileDestination);
-        console.log('logoDestination', logoDestination);
 
-        extractFile(filePath, fileDestination);
-        moveFile(logoPath, logoDestination, fileDestination);
+        extractFile(file.path, fileDestination);
+        moveFile(logo.path, logoDestination, fileDestination);
+
+        let originalFilename = file.originalname.split(".")[0];
 
         let { name, description, fundingGoal, gameJamId, developerId } = request.body;
         let game = await Game.create({
